95affbf0541cd4ae14b3bc11ccda9a3d579f0489
Sumit Ridhal <sumitridhal@gmail.com>
2020-05-03 20:01:26 +0530

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.scss' ]
+})
+export class AppComponent  {
+  @HostBinding('class') classes = 'app';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
