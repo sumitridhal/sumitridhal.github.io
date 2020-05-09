2effdd831b5709a5822f9e3c3825e15260aa0ae2
Sumit Ridhal <sumitridhal@gmail.com>
2020-05-09 14:05:45 +0530

    Auto-generated commit

--- src/app/app-routing.module.ts
+++
@@ -1,8 +0,0 @@
-
-
-// const routes: Routes = [
-//     { path: '', pathMatch: 'full', redirectTo: '/home' },
-//     { path: 'home', component: HomeComponent },
-//     { path: 'about', component: AboutComponent },
-//     { path: 'contact', component: ContactComponent },
-// ];
