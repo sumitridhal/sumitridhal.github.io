60b0e9cada058371b7c72ddf012107f611ab2b56
Sumit Ridhal <sridhal@redhat.com>
2026-05-09 17:04:01 +0530

    docs(src/consts.ts): add new metadata constant for About page

--- src/consts.ts
+++ src/consts.ts
@@ -13,6 +13,11 @@ export const HOME: Metadata = {
   DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
 };
 
+export const ABOUT: Metadata = {
+  TITLE: "About",
+  DESCRIPTION: "A short introduction and selected writing.",
+};
+
 export const BLOG: Metadata = {
   TITLE: "Blog",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
