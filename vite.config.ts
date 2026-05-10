100d9a897aa0ea76dc8abd5ec06b8a0ee1a6a00f
Sumit Ridhal <sridhal@redhat.com>
2026-05-10 14:10:37 +0530

    feat(update): update eslint and vite dependencies, add MDX support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'node:path'
+import mdx from '@mdx-js/rollup'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    { enforce: 'pre', ...mdx({ jsxImportSource: 'react' }) },
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
