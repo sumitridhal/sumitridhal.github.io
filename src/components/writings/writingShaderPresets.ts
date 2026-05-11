712a23e5a542e203c1123dbe7c89fe431da8794a
Sumit Ridhal <sridhal@redhat.com>
2026-05-11 09:49:55 +0530

    feat(writing): add writing shader presets

---
+++ src/components/writings/writingShaderPresets.ts
@@ -0,0 +1,40 @@
+import analyticRaySphere from '@/shaders/writing-demos/analytic-ray-sphere.frag?raw'
+import ditherBanding from '@/shaders/writing-demos/dither-banding.frag?raw'
+import fragmentPipelineUv from '@/shaders/writing-demos/fragment-pipeline-uv.frag?raw'
+import fullscreenPassVignette from '@/shaders/writing-demos/fullscreen-pass-vignette.frag?raw'
+import gridTransform2d from '@/shaders/writing-demos/grid-transform-2d.frag?raw'
+import halftoneDots from '@/shaders/writing-demos/halftone-dots.frag?raw'
+import lightFalloff from '@/shaders/writing-demos/light-falloff.frag?raw'
+import raymarchSphere from '@/shaders/writing-demos/raymarch-sphere.frag?raw'
+import smoothstepDerivatives from '@/shaders/writing-demos/smoothstep-derivatives.frag?raw'
+import sobelEdges from '@/shaders/writing-demos/sobel-edges.frag?raw'
+import toneQuantize from '@/shaders/writing-demos/tone-quantize.frag?raw'
+import valueNoiseFbm from '@/shaders/writing-demos/value-noise-fbm.frag?raw'
+import webgpuMentalModel from '@/shaders/writing-demos/webgpu-mental-model.frag?raw'
+
+export const WRITING_SHADER_PRESETS = {
+  ditherBanding,
+  halftoneDots,
+  lightFalloff,
+  sobelEdges,
+  fragmentPipelineUv,
+  fullscreenPassVignette,
+  valueNoiseFbm,
+  raymarchSphere,
+  analyticRaySphere,
+  smoothstepDerivatives,
+  toneQuantize,
+  gridTransform2d,
+  webgpuMentalModel,
+} as const
+
+export type WritingShaderPresetSingle = keyof typeof WRITING_SHADER_PRESETS
+
+export type WritingShaderPreset = WritingShaderPresetSingle | 'pingPongFeedback'
+
+export function getWritingShaderFragmentSource(
+  preset: WritingShaderPreset,
+): string | null {
+  if (preset === 'pingPongFeedback') return null
+  return WRITING_SHADER_PRESETS[preset]
+}
