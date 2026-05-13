46e2dfc4063ffaca4c6e2b6f2d07905f1c28e295
Sumit Ridhal <sridhal@redhat.com>
2026-05-13 12:33:25 +0530

    refactor(src/App.tsx): consolidated routes and removed experiment page

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { AppShell } from '@/components/AppShell'
 import { AboutPage } from '@/pages/AboutPage'
-import { ExperimentsPage } from '@/pages/ExperimentsPage'
 import { HomePage } from '@/pages/HomePage'
 import { ProjectPage } from '@/pages/ProjectPage'
 import { WritingPage } from '@/pages/WritingPage'
@@ -14,7 +13,6 @@ export default function App() {
       <Route path="/" element={<AppShell />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
-        <Route path="experiments" element={<ExperimentsPage />} />
         <Route path="writing" element={<WritingsGalleryPage />} />
         <Route path="writing/:slug" element={<WritingPage />} />
         <Route path="work/:slug" element={<ProjectPage />} />
