9309b2d20c1e8f4db1acee00d1d8f344b255d61a
Sumit Ridhal <sridhal@redhat.com>
2026-05-14 16:35:45 +0530

    feat(writings): add shared Object & Archive hero CDN assets

---
+++ src/components/writings/writingObjectArchiveHeroCdn.ts
@@ -0,0 +1,7 @@
+/** Shared Object & Archive hero CDN assets (Part 1 split + wordmark). */
+export const OA_HERO_IMG_LEFT =
+  'https://objectandarchive.com/cdn/shop/files/Etudedeforet.jpg?v=1775498766&width=800'
+export const OA_HERO_IMG_RIGHT =
+  'https://objectandarchive.com/cdn/shop/files/monet_detail.png?v=1777132945&width=800'
+export const OA_HERO_LOGO_SVG =
+  'https://objectandarchive.com/cdn/shop/files/oa-white.svg?v=1774631393&width=2000'
