b10e75922f5a8f40e2108478a20f529bf5c894de
Sumit Ridhal <sridhal@redhat.com>
2026-05-16 15:03:26 +0530

    feat(data): update banner images for home section

--- src/data/homeSectionBanners.ts
+++ src/data/homeSectionBanners.ts
@@ -2,7 +2,7 @@ import type { HomeThemeId } from '@/utils/homeTheme'
 
 export const HOME_SECTION_BANNERS = {
   experiments:
-    'https://objectandarchive.com/cdn/shop/files/firmament-banner.png?v=1775309298&width=1200',
+    'https://objectandarchive.com/cdn/shop/files/firmament-banner.png?v=1775309298&width=1800',
   writings:
     'https://objectandarchive.com/cdn/shop/files/bottom-banner2.jpg?v=1774725463&width=2000',
   books:
