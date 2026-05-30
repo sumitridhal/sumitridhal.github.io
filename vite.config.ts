2f0f85a197cc885d6b319270e3921950d3f986e9
Sumit Ridhal <sridhal@redhat.com>
2026-05-30 14:49:26 +0530

    feat: add Tailwind CSS support and updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import path from 'node:path'
 import mdx from '@mdx-js/rollup'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     { enforce: 'pre', ...mdx({ jsxImportSource: 'react' }) },
     react(),
   ],
